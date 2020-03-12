970e0adadc619af477d2144ffab7414e12c8af13
MarceArratia <marratiaf@hotmail.com>
2020-03-12 11:01:25 -0300

    mostrando vistas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ArtboardUno from './component/ArtboardUno';
-import ArtboardDos from './component/ArtboardDos';
+
 import './App.css';
 
 
 function App() {
+
+
+
+
   return (
     <div className="App">
  <ArtboardUno />
-<ArtboardDos/>
+
     </div>
   );
 }
