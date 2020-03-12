f1bb1da813c146e06888efe5f154cbcbf653fe19
MarceArratia <marratiaf@hotmail.com>
2020-03-12 02:51:02 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ArtboardUno from './component/ArtboardUno';
+import ArtboardDos from './component/ArtboardDos';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+ <ArtboardUno />
+<ArtboardDos/>
+    </div>
+  );
+}
+
+export default App;
